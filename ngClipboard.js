8af5bd740d7416b79ff5712ae6bde8c5309c4a33
Nicolás Valenzuela <nico-val@users.noreply.github.com>
2015-10-14 01:38:56 -0300

    Update ngClipboard.js
    Added "Copy" factory that sends a string to clipboard. Again, just a prototype.

--- ngClipboard.js
+++ ngClipboard.js
@@ -1,5 +1,26 @@
 angular.module('ngClipboard', [])
 
+    .factory('copy', function($compile) {
+        return function(element, scope){
+
+            var newDirective = angular.element('<span id="ngCopyId">'+element+'</span>');
+            var test = $('body').append($compile(newDirective)(scope));
+            var test2 = $('#ngCopyId');
+            var range = document.createRange();
+            range.selectNode(test2[0]);
+            window.getSelection().removeAllRanges();
+            window.getSelection().addRange(range);
+            var successful = document.execCommand('copy');
+
+            var msg = successful ? 'successful' : 'unsuccessful';
+            console.log('Copying text command was ' + msg);
+            window.getSelection().removeAllRanges();
+
+            newDirective.remove();
+
+        };
+    })
+
     .directive('ngCopyable', function() {
         return {
             restrict: 'A',
@@ -7,8 +28,12 @@ angular.module('ngClipboard', [])
         };
         function link(scope, element, attrs) {
             element.bind('click',function(){
+
                 var range = document.createRange();
-                range.selectNode(element[0]);
+                var other = element[0];
+                var asd = $('#ngCopyId');
+                range.selectNode(asd[0]);
+                window.getSelection().removeAllRanges();
                 window.getSelection().addRange(range);
                 var successful = document.execCommand('copy');
 
@@ -19,3 +44,4 @@ angular.module('ngClipboard', [])
         }
 
     });
+
